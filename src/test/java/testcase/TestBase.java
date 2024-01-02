076cd8ed7b2983e6ebbb02d6ba67d064cdd66868
ayaMohamed186 <37931347+ayaMohamed186@users.noreply.github.com>
2024-01-03 01:25:11 +0200

    Allure report update

--- src/test/java/testcase/TestBase.java
+++ src/test/java/testcase/TestBase.java
@@ -1,19 +1,65 @@
 package testcase;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.github.javafaker.Faker;
 import common.MyScreenRecorder;
 import drivers.DriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import static drivers.DriverHolder.setDriver;
 
 public class TestBase {
     WebDriver driver;
 
+    protected static ExtentSparkReporter htmlReporter;
+    protected static ExtentReports extent;
+    protected static ExtentTest test;
+    private static String PROJECT_NAME = null;
+    private static String PROJECT_URL = null;
+    static Properties prop;
+    static FileInputStream readProperty;
+
+
     @BeforeSuite
     public void defineSuite() throws Exception {
-        MyScreenRecorder.startRecording("E2E Swag Labs");
+        MyScreenRecorder.startRecording("E2E Swag Labs ");
+        // initialize the HtmlReporter
+        htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/testReport.html");
+
+        //initialize ExtentReports and attach the HtmlReporter
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        setProjectDetails();
+
+        // initialize test
+        test = extent.createTest(PROJECT_NAME + " Test Automation Project");
+
+        //configuration items to change the look and fee add content, manage tests etc
+        htmlReporter.config().setDocumentTitle(PROJECT_NAME + " Test Automation Report");
+        htmlReporter.config().setReportName(PROJECT_NAME + " Test Report");
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+    }
+
+    private void setProjectDetails() throws IOException, IOException {
+        // TODO: Step1: define object of properties file
+        readProperty = new FileInputStream(
+                System.getProperty("user.dir") + "/src/test/resources/properties/environment.properties");
+        prop = new Properties();
+        prop.load(readProperty);
+
+        // define project name from properties file
+        PROJECT_NAME = prop.getProperty("projectName");
+        PROJECT_URL = prop.getProperty("url");
     }
 
     @Parameters("browser")
@@ -23,7 +69,7 @@ public class TestBase {
         driver = DriverFactory.getNewInstance(browser);
         setDriver(driver);
 
-        driver.get("https://www.saucedemo.com/v1/");
+        driver.get(PROJECT_URL);
     }
 
     @AfterTest
